4958dad86982a39af7e43b10fdb1279c252639e4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-11-01 16:29:57 +0100

    fix typo in Transaction entity

--- src/main/java/com/youcode/bankify/entity/Transaction.java
+++ src/main/java/com/youcode/bankify/entity/Transaction.java
@@ -27,4 +27,8 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "bank_account_id", nullable = false)
     private BankAccount bankAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
